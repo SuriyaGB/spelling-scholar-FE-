acac0ca2f16cea07766f65c72dc48bc8094625c5
SuriyaGB <suriya@bumblebeecreatives.com>
2026-06-01 12:13:46 +0530

    feat: use env variable VITE_SENTRY_DSN in code and GitHub pipeline

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,9 @@ import App from "./App.tsx";
 import "./index.css";
 
 Sentry.init({
-  dsn: "https://fce6973a772d1ae9d6be2dab133a8fc9@o4511488773062656.ingest.us.sentry.io/4511488800456704",
+  dsn: import.meta.env.VITE_SENTRY_DSN,
   sendDefaultPii: true
 });
-
-// Temporary test error to verify Sentry integration
-throw new Error("Sentry test error");
 
 createRoot(document.getElementById("root")!).render(
   <>
